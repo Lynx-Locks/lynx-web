7b2e6dbf85fd19baebde4966b27a6bc5e3f3883e
Adrian Jendo <ajendo@uwaterloo.ca>
2024-01-26 00:31:43 -0500

    Add modal component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,42 +4,42 @@ import AdminTable from "@/components/adminTable/table";
 import TableButtons from "@/components/tableButtons/tableButtons";
 import User from "@/types/user";
 
-export default function Home() {
-  const users: User[] = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "leannegraham@lynxlocks.com",
-      timeIn: "00:00:00",
-      date: "2024/01/01",
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "ervinhowell@lynxlocks.com",
-      timeIn: "00:00:00",
-      date: "2024/01/01",
-    },
-    {
-      id: 3,
-      name: "Clementine Bauch",
-      username: "Samantha",
-      email: "clementinebauch@lynxlocks.com",
-      timeIn: "00:00:00",
-      date: "2024/01/01",
-    },
-    {
-      id: 4,
-      name: "Patricia Lebsack",
-      username: "Karianne",
-      email: "patricialebsack@lynxlocks.com",
-      timeIn: "00:00:00",
-      date: "2024/01/01",
-    },
-  ];
+const users: User[] = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "leannegraham@lynxlocks.com",
+    timeIn: "00:00:00",
+    date: "2024/01/01",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "ervinhowell@lynxlocks.com",
+    timeIn: "00:00:00",
+    date: "2024/01/01",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "clementinebauch@lynxlocks.com",
+    timeIn: "00:00:00",
+    date: "2024/01/01",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "patricialebsack@lynxlocks.com",
+    timeIn: "00:00:00",
+    date: "2024/01/01",
+  },
+];
 
+export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <h1 className={styles.homeHeader}>Users</h1>
