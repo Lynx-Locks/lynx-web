8fec30a52034e1eff78fe370b1854dd80c43454e
Adrian Jendo <ajendo@uwaterloo.ca>
2024-01-31 00:37:01 -0500

    Implement better search dropdown

--- frontend/src/components/searchDropdown/searchDropdown.tsx
+++ frontend/src/components/searchDropdown/searchDropdown.tsx
@@ -2,66 +2,39 @@
 
 import { useEffect, useState } from "react";
 import styles from "./searchDropdown.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Select from "react-select";
+
+interface Options {
+  label: string;
+  value: string;
+}
 
 export default function SearchDropdown({
   options,
   placeholder,
+  subheader,
+  isMulti = false,
 }: {
-  options: string[];
+  options: Options[];
   placeholder: string;
+  subheader: string;
+  isMulti?: boolean;
 }) {
-  const [searchOptions, setSearchOptions] = useState<string[]>([]);
-  const [searchActive, setSearchActive] = useState(false);
-  const [selected, setSelected] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setSearchOptions(options);
-  }, [setSearchOptions, options]);
-
-  const filterSearchOptions = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const filteredOptions = options.filter((option) => {
-      return option.toLowerCase().includes(e.target.value.toLowerCase());
-    });
-    setSearchOptions(filteredOptions);
-    setSelected(e.target.value);
-  };
-
-  const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" || e.key === "Escape") {
-      e.currentTarget.blur();
-    }
-  };
+  useEffect(() => setIsMounted(true), []);
 
   return (
     <div className={styles.searchDropdownContainer}>
-      <span className={styles.searchDropdownIcon}>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-      </span>
-      <input
-        className={styles.searchDropdownInput}
-        type="text"
-        placeholder={placeholder}
-        onFocus={() => setSearchActive(true)}
-        onBlur={() => setSearchActive(false)}
-        onChange={filterSearchOptions}
-        onKeyDown={keyDown}
-        value={selected}
-      />
-      {searchActive && (
-        <ul className={styles.dropdownSelect} id="dropdownOptions">
-          {searchOptions.map((searchOption) => (
-            <li
-              key={searchOption}
-              className={styles.dropdownSelectItem}
-              value={searchOption}
-              onMouseDown={() => setSelected(searchOption)}
-            >
-              {searchOption}
-            </li>
-          ))}
-        </ul>
+      <h2 className={styles.subheader}>{subheader}</h2>
+      {isMounted && (
+        <Select
+          className={styles.selectDropdown}
+          options={options}
+          name={subheader}
+          placeholder={placeholder}
+          isMulti={isMulti}
+        />
       )}
     </div>
   );
