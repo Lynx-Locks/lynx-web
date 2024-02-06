4661f11fe33112e8cfeb7863381c8a2db87f1ec0
Adrian Jendo <ajendo@uwaterloo.ca>
2024-02-06 00:14:33 -0500

    Implement settings modal

--- frontend/src/components/adminTable/table.tsx
+++ frontend/src/components/adminTable/table.tsx
@@ -6,9 +6,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import Modal from "@/components/modal/modal";
+import { SubmitButon } from "@/components/button/button";
 
 export default function AdminTable({ users }: { users: User[] }) {
-  const [settingsModal, setSettingsModal] = useState(-1);
+  const [settingsModal, setSettingsModal] = useState(1);
   const handleClickSettings = (idx: number) => {
     setSettingsModal(idx);
   };
@@ -65,7 +66,43 @@ export default function AdminTable({ users }: { users: User[] }) {
       {users.length === 0 && (
         <h1 className={styles.emptyTable}>Could not display any users</h1>
       )}
-      {settingsModal >= 0 && <Modal setShowModal={closeModal} />}
+      {settingsModal >= 0 && (
+        <Modal
+          setShowModal={closeModal}
+          title={`Settings for ${users[settingsModal].name}`}
+          content={
+            <div className={styles.settingsModal}>
+              <div className={styles.settingsInputContainer}>
+                Name:{" "}
+                <input
+                  className={styles.settingsInput}
+                  type="text"
+                  value={users[settingsModal].name}
+                />
+              </div>
+              <div className={styles.settingsInputContainer}>
+                Email:{" "}
+                <input
+                  className={styles.settingsInput}
+                  type="text"
+                  value={users[settingsModal].email}
+                />
+              </div>
+              <div className={styles.settingsButtonGroup}>
+                <button className={styles.settingsButton}>Update role</button>
+                <button className={styles.settingsButton}>
+                  Revoke & Regenerate credentials
+                </button>
+                <button className={styles.settingsButton}>Delete User</button>
+              </div>
+              <SubmitButon
+                text="Submit Changes"
+                onClick={() => setSettingsModal(-1)}
+              />
+            </div>
+          }
+        />
+      )}
     </div>
   );
 }
