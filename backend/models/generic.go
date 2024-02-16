e78b3aa91cef226c78a85cbfcb6a489073620b1c
Adam Barroso <abarroso@uwaterloo.ca>
2024-02-16 18:37:28 -0500

    Fix error causing post req to fail on false is_admin creates

--- backend/models/generic.go
+++ backend/models/generic.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Common struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
+	Id        uint           `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
