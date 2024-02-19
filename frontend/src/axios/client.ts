eaca4c000e319f5ebb47962cf2d6cff0933a45cd
Adrian Jendo <ajendo@uwaterloo.ca>
2024-02-19 13:37:58 -0500

    Run prettier

--- frontend/src/axios/client.ts
+++ frontend/src/axios/client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: process.env.API_BASE_URL || "http://localhost:5001"
-})
-
+  baseURL: process.env.API_BASE_URL || "http://localhost:5001",
+});
 
 export default client;
