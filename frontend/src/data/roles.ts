a9c13a281c8d1fb19acab8515145eae45f29c938
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-09 11:19:20 -0500

    Run prettier

--- frontend/src/data/roles.ts
+++ frontend/src/data/roles.ts
@@ -1,13 +1,13 @@
-import axios from "@/axios/client"
+import axios from "@/axios/client";
 import { Role } from "@/types/role";
 import { Options } from "@/types/selectOptions";
 
-export const getRoleOptions = async () : Promise<Options[]> => {
-	const resp = await axios.get("/doors");
-	const roles : Role[] = resp.data;
+export const getRoleOptions = async (): Promise<Options[]> => {
+  const resp = await axios.get("/doors");
+  const roles: Role[] = resp.data;
 
-	return roles.map((r) => ({
-		label: r.name,
-		value: r.id.toString()
-	}));
-}
+  return roles.map((r) => ({
+    label: r.name,
+    value: r.id.toString(),
+  }));
+};
