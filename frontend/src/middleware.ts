9df4ec77cbb1872635f55f759d4831b347326d54
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-13 00:34:07 -0400

    Run prettier

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,24 +2,27 @@ import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 // export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/", "/admin"] }
+export const config = { matcher: ["/", "/admin"] };
 
 export default withAuth(
-	function middleware(request: NextRequestWithAuth) {
-		if (request.nextUrl.pathname.startsWith("/admin") && !request.nextauth.token?.isAdmin) {
-			return NextResponse.rewrite(new URL("/denied", request.url));
-		}
-	},
-	{ 
-	callbacks: {
-		authorized: ({ token }) => !!token,
-	},
-	pages: {
-		signIn: "/login",
-		signOut: "/login",
-	},
-	} 
-)
+  function middleware(request: NextRequestWithAuth) {
+    if (
+      request.nextUrl.pathname.startsWith("/admin") &&
+      !request.nextauth.token?.isAdmin
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/login",
+      signOut: "/login",
+    },
+  },
+);
 
 // 	{
 //   // Must match with /api/auth/[...nextauth]/options.ts
--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -1,32 +1,31 @@
-
 import { DefaultSession, DefaultUser } from "next-auth";
 import { JWT, DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-	interface Session {
-		user: {
-			id: string,
-			isAdmin: boolean,
-			email: string,
-			name: string,
-			timeIn: string,
-  		lastDateIn: string,
-		} & DefaultSession
-	}
+  interface Session {
+    user: {
+      id: string;
+      isAdmin: boolean;
+      email: string;
+      name: string;
+      timeIn: string;
+      lastDateIn: string;
+    } & DefaultSession;
+  }
 
-	interface User extends DefaultUser {
-		isAdmin: boolean,
-		email: string,
-		name: string,
-		timeIn: string,
-  	lastDateIn: string,
-	}
+  interface User extends DefaultUser {
+    isAdmin: boolean;
+    email: string;
+    name: string;
+    timeIn: string;
+    lastDateIn: string;
+  }
 }
 
 declare module "next-auth/jwt" {
-	interface JWT extends DefaultJWT {
-		isAdmin: boolean,
-		email: string,
-		name: string,
-	}
+  interface JWT extends DefaultJWT {
+    isAdmin: boolean;
+    email: string;
+    name: string;
+  }
 }
