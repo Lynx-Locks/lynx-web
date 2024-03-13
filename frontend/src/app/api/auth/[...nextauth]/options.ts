fd85d2ad6c50585e466748b3bb3ec71712e8b9af
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-13 00:34:07 -0400

    Run prettier

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,48 +1,47 @@
-import type {NextAuthOptions} from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "@/axios/client";
 import { PublicKeyCredentialRequestOptionsJSON } from "@simplewebauthn/types";
 import { startAuthentication } from "@simplewebauthn/browser";
 
 export const options: NextAuthOptions = {
-	providers: [
-		CredentialsProvider({
-			id: 'login',
-			name: "Credentials",
-			credentials: {
-				email: {
-					label: "Email",
-					type: "text",
-				},
-			},
-			async authorize(credentials, _req) {
-				const userResp = await axios.get("/users/login", {
-					params: {
-						email: credentials?.email,
-					},
-				});
-				const user = userResp.data;
-				return user;
-			}
-		}
-		)
-	],
-	pages: {
-		signIn: "/login",
-		signOut: "/login",
-	},
-	callbacks: {
-		async jwt({ token, user }) {
-			if (user) {
-				token.isAdmin = user.isAdmin;
-			}
-			return token;
-		},
-		async session({ session, token }) {
-			if (session?.user) {
-				session.user.isAdmin = token.isAdmin;
-			}
-			return session
-		}
-	}
-}
+  providers: [
+    CredentialsProvider({
+      id: "login",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+      },
+      async authorize(credentials, _req) {
+        const userResp = await axios.get("/users/login", {
+          params: {
+            email: credentials?.email,
+          },
+        });
+        const user = userResp.data;
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.isAdmin = user.isAdmin;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
+  },
+};
