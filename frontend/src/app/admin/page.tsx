c8f709dc377cef246c73c163621d253d2b9b3b68
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-16 14:11:57 -0400

    Implement admin dashboard link logic

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import Dashboard from "@/components/adminDashboard/adminDashboard";
 import Navbar from "@/components/navbar/navbar";
-import { useEffect, useState } from "react";
 
 export default function Admin() {
-  const [name, setName] = useState<string>("");
-  useEffect(() => {
-    if (localStorage.getItem("name")) {
-      setName(String(localStorage.getItem("name")));
-    }
-  }, []);
-
   return (
     <div>
-      <Navbar name={name} />
+      <Navbar page="admin" />
       <Dashboard />
     </div>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import Navbar from "@/components/navbar/navbar";
-import { useEffect, useState } from "react";
 import Dashboard from "@/components/userDashboard/userDashboard";
 
 export default function Home() {
-  const [name, setName] = useState<string>("");
-  useEffect(() => {
-    if (localStorage.getItem("name")) {
-      setName(String(localStorage.getItem("name")));
-    }
-  }, []);
-
   return (
     <div>
-      <Navbar name={name} />
+      <Navbar page="portal" />
       <Dashboard />
     </div>
   );
